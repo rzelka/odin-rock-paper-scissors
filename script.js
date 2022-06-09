1a6b95812dd083b85a28ba77c6ee5b4a247de281
pchajko <pchajko@gmail.com>
2022-06-09 20:17:59 +0200

    Create 5 rounds of game.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//define choices in game
-
-const choice = ['rock','paper','scissors'];
-
 //choose random value from available choices
 
 function computerPlay() { 
@@ -11,47 +7,93 @@ function computerPlay() {
 //define player selection
 
 function userPlay() {
+
     let input = prompt('Choose your weapon: rock, paper or scissors!');
-    
     if (input === null) {
-        return 'escape';
+        return 'escape'
     } else if (input.toLowerCase() === 'rock' || input.toLowerCase() === 'paper' || input.toLowerCase() === 'scissors') {
-       return input.toLowerCase();
+       return input.toLowerCase()
     } else {
         do {
             input = prompt('Can\'t do that. Choose between available weapons (rock, paper, scissors).');
             if (input === null) {
-                return 'escape';
+                return 'escape'
             } else if (input.toLowerCase() === 'rock' || input.toLowerCase() === 'paper' || input.toLowerCase() === 'scissors') {
-                return input.toLowerCase();  
+                return input.toLowerCase()
             }}
         while (input === '' || input.toLowerCase() !== 'rock' || input.toLowerCase() !== 'paper' || input.toLowerCase() !== 'scissors')}
 }
-//define players move
-
-let playerSelection  = userPlay();
-let computerSelection = computerPlay();
 
 //play single game
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection,computerSelection) {
+
     if (playerSelection === 'rock' && computerSelection === 'rock'||
         playerSelection === 'paper' && computerSelection === 'paper'||
         playerSelection === 'scissors' && computerSelection === 'scissors') {
-        return ('It\'s a tie! None wins.')
+        return 'It\'s a tie! None wins.'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'||
     playerSelection === 'paper' && computerSelection === 'rock'||
     playerSelection === 'scissors' && computerSelection === 'paper') {
-    return ('You won!')
+        return 'You won!'
     } else if (playerSelection === 'escape') {
-    return ('You escaped the battle!')
+        return 'You escaped the battle!'
     } else {
-    return ('You lost!')
+        return 'You lost!'
+    } 
+}
+
+//keeping score
+
+function whoWon() {
+    if (playRound(playerSelection,computerSelection) === 'You won!') {
+        playerScore++;
+    } else if (playRound(playerSelection,computerSelection) === 'You lost!') {
+        computerScore++;
+    } else {
+        return
     }
 }
 
-//show results
+//play 5 games in a row
 
-console.log("Computer pick: " + computerSelection);
-console.log("Player pick: " + playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+
+    for (i = 0; i < 5; i++) {
+        computerSelection = computerPlay();
+        playerSelection = userPlay();
+        playRound(playerSelection,computerSelection);
+        whoWon();
+        console.log('Computer pick: ' + computerSelection);
+        console.log('Player pick: ' + playerSelection);
+        console.log('Computer score: ' + computerScore);
+        console.log('Player score: ' + playerScore);
+    }
+}
+
+//defines a winner
+
+function winner(computerScore, playerScore) {
+    if (playerScore>computerScore) {
+        return 'Congrats! You win against the machine!'
+    }
+    if (playerScore<computerScore) {
+        return 'I\'m sorry, you lose. Machine beat you!'
+    } 
+}
+
+//define choices in game
+
+const choice = ['rock','paper','scissors'];
+
+//define moves of the players
+
+let computerSelection;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+//call a game
+
+game();
+console.log(winner(computerScore, playerScore));
